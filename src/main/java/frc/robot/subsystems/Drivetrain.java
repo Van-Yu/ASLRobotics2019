67c056278076cc310046bc5b4826da7e95f1960c
Lorenzo <awesomelorenzokwenda@gmail.com>
2019-02-06 15:36:01 +0000

    Update Drivetrain.java
    MADE ALL TALONSRX

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -15,6 +15,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  private WPI_TalonSRX FRONT_LEFT_DRIVE_MOTOR;
+  private WPI_TalonSRX BACK_LEFT_DRIVE_MOTOR;
+  private WPI_TalonSRX FRONT_RIGHT_DRIVE_MOTOR;
+  private WPI_TalonSRX BACK_RIGHT_DRIVE_MOTOR;
+
+  public Drivetrain(){
+    FRONT_LEFT_DRIVE_MOTOR = new WPI_TalonSRX(RobotMap.FRONT_LEFT_DRIVE_MOTOR);
+    BACK_LEFT_DRIVE_MOTOR = new WPI_TalonSRX(RobotMap.BACK_LEFT_DRIVE_MOTOR);
+    FRONT_RIGHT_DRIVE_MOTOR = new WPI_TalonSRX(RobotMap.FRONT_RIGHT_DRIVE_MOTOR);
+    BACK_RIGHT_DRIVE_MOTOR = new WPI_TalonSRX(RobotMap.BACK_RIGHT_DRIVE_MOTOR);
+
+  }
 
   @Override
   public void initDefaultCommand() {
