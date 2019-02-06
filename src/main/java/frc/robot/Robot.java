5c974fe7901c3929dd3880fa9c947a337adbf760
Lorenzo <awesomelorenzokwenda@gmail.com>
2019-02-06 16:19:32 +0000

    Removed things in robot.jvaa

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.Drivetrain;
+//LORENZO DRIVETRAIN ADD - import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -25,8 +25,8 @@ import frc.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  //LORENZO DRIVETRAIN ADD - public static Drivetrain DrivetrainSubsystem;
   public static OI oi;
-  public static Drivetrain DrivetrainSubsystem;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -41,7 +41,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
-    DrivetrainSubsystem = new Drivetrain();
+    //LORENZO DRIVETRAIN ADD - DrivetrainSubsystem = new Drivetrain();
   }
 
   /**
