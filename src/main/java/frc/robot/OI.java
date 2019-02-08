0d0acc9bca1e7295bfc195bad518d548ce7d9266
Sarah Covey <sarah_covey@asl.org>
2019-02-08 10:17:30 +0000

    eddited controlls in OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -20,8 +20,7 @@ public class OI {
   public final Joystick DRIVER = new Joystick(RobotMap.DRIVER_CONTROL_PORT);
   public final Joystick OPERATOR = new Joystick(RobotMap.OPERATOR_CONTROL_PORT);
 
-  private final JoystickButton DEPLOY_ARM = new JoystickButton(OPERATOR, RobotMap.DEPLOY_ARM_BUTTON_PORT);
-  private final JoystickButton RETRACT_ARM = new JoystickButton(OPERATOR, RobotMap.RETRACT_ARM_BUTTON_PORT);
+  private final JoystickButton CARGO_BUTTON = new JoystickButton(OPERATOR, RobotMap.CARGO_BUTTON_PORT);
 
   private final JoystickButton HATCH_BUTTON = new JoystickButton(OPERATOR, RobotMap.HATCH_BUTTON_PORT);
 
@@ -29,15 +28,14 @@ public class OI {
   private final JoystickButton ELEVATOR_DOWN_BUTTON = new JoystickButton(OPERATOR, RobotMap.ELEVATOR_DOWN_BUTTON_PORT);
 
   public OI() {
-    DEPLOY_ARM.whileHeld(new StartWheels());
-    DEPLOY_ARM.whenPressed(new ArmUp());
-
-    RETRACT_ARM.whenPressed(new ArmDown());
+    CARGO_BUTTON.whileHeld(new StartWheels());
+    CARGO_BUTTON.whenPressed(new ArmUp());
+    CARGO_BUTTON.whenReleased(new ArmDown());
 
     HATCH_BUTTON.whenPressed(new HatchCommand());
 
-    ELEVATOR_UP_BUTTON.whenPressed(new ElevatorIncrement());
-    ELEVATOR_DOWN_BUTTON.whenPressed(new ElevatorDecrement());
+    // ELEVATOR_UP_BUTTON.whenPressed(new ElevatorIncrement());
+    // ELEVATOR_DOWN_BUTTON.whenPressed(new ElevatorDecrement());
   }
 
 
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -16,15 +16,23 @@ package frc.robot;
 public class RobotMap {
 
   // OI Ports
+    // joysticks
   public static final int DRIVER_CONTROL_PORT = 0;
   public static final int OPERATOR_CONTROL_PORT = 1;
 
-  public static final int DEPLOY_ARM_BUTTON_PORT = 1;
-  public static final int RETRACT_ARM_BUTTON_PORT = 2;
+    // buttons
+  public static final int CARGO_BUTTON_PORT = 1;
   public static final int HATCH_BUTTON_PORT = 3;
+
+  public static final int DEPLOY_CARGO_BUTTON_PORT = 4;
 
   public static final int ELEVATOR_UP_BUTTON_PORT = 5;
   public static final int ELEVATOR_DOWN_BUTTON_PORT = 6;
+
+    // axes
+  public static final int DRIVE_FORWARD_AXIS = 1;
+  public static final int DRIVE_TURN_AXIS = 4;
+
 
   // MOTORS
   public static final int FRONT_LEFT_DRIVE_MOTOR = 0;
