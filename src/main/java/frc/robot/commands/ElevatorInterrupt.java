4e760567099e8bc26c8ee117208f0aa3ea32ab56
Sarah Covey <sarah_covey@asl.org>
2019-02-08 14:45:12 +0000

    added elevator controls

--- src/main/java/frc/robot/commands/ElevatorInterrupt.java
+++ src/main/java/frc/robot/commands/ElevatorInterrupt.java
@@ -9,44 +9,37 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 
-public class ElevatorMoveCommand extends Command {
-  public ElevatorMoveCommand() {
+public class ElevatorInterrupt extends Command {
+  public ElevatorInterrupt() {
     // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
     requires(Robot.ELEVATOR);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.ELEVATOR.stopMotor();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    // Robot.ELEVATOR.setMotorSpeed(-Robot.oi.OPERATOR.getRawAxis(5));
-    Robot.ELEVATOR.setMotorSpeed(Robot.oi.OPERATOR.getRawAxis(RobotMap.OPERATOR_ELEVATOR_AXIS));
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.ELEVATOR.stopMotor();
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    end();
   }
 }
